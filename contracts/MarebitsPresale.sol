af947be75a6463bbc6b7b899b4d835ef0d2b1d15
Corpulent Brony <corpulent@brony.email>
2021-09-15 23:36:22 -0400

    Lots of changes, writing a proper extension of the contract to support minPurchase, testing ongoing...

--- contracts/MarebitsPresale.sol
+++ contracts/MarebitsPresale.sol
@@ -5,11 +5,62 @@ import "@openzeppelin/contracts/crowdsale/validation/CappedCrowdsale.sol";
 import "@openzeppelin/contracts/crowdsale/validation/TimedCrowdsale.sol";
 import "@openzeppelin/contracts/crowdsale/distribution/PostDeliveryCrowdsale.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
+/**
+ * @title MarebitsPresale
+ * @dev Timed, Capped, PostDelivery Crowdsale with a minimum purchase, too ^:)
+ */
 contract MarebitsPresale is Crowdsale, CappedCrowdsale, TimedCrowdsale, PostDeliveryCrowdsale {
-	constructor(uint256 rate, address payable wallet, IERC20 token, uint256 cap, uint256 openingTime, uint256 closingTime) 
-		PostDeliveryCrowdsale() TimedCrowdsale(openingTime, closingTime) CappedCrowdsale(cap) Crowdsale(rate, wallet, token) public
-	{
+	using SafeMath for uint256;
 
+	uint256 private _minPurchase;
+
+	/**
+	 * @dev Constructor, takes minimum amount of wei accepted in the crowdsale.
+	 * @param rate Amount of tokens purchased per ETH
+	 * @param minPurchase Minimum amount of wei to be contributed
+	 */
+	constructor(uint256 rate, address payable wallet, IERC20 token, uint256 cap, uint256 openingTime, uint256 closingTime, uint256 minPurchase) 
+		PostDeliveryCrowdsale() TimedCrowdsale(openingTime, closingTime) CappedCrowdsale(cap) Crowdsale(rate, wallet, token) public {
+		_minPurchase = minPurchase;
+	}
+
+	/**
+	 * @return the best pony.
+	 */
+	function bestPony() public view returns (string memory) {
+		return "Twilight Sparkle is the best pony ever, she's really smart and likes books.";
+	}
+
+	/**
+	 * @return the minimum purchase of the crowdsale.
+	 */
+	function minPurchase() public view returns (uint256) {
+		return _minPurchase;
+	}
+
+	// temporary override for testing purposes only, remove in production!
+	function _deliverTokens(address beneficiary, uint256 tokenAmount) internal {
+		token().transfer(beneficiary, tokenAmount);
+	}
+
+	/**
+	 * @dev Override to extend the way in which ether is converted to tokens.
+	 * @param weiAmount Value in wei to be converted into tokens
+	 * @return Number of tokens that can be purchased with the specified _weiAmount
+	 */
+	function _getTokenAmount(uint256 weiAmount) internal view returns (uint256) {
+		return weiAmount.mul(rate()).div(1e18);
+	}
+
+	/**
+	 * @dev Extend parent behavior requiring purchase to respect the minimum purchase.
+	 * @param beneficiary Token purchaser
+	 * @param weiAmount Amount of wei contributed
+	 */
+	function _preValidatePurchase(address beneficiary, uint256 weiAmount) internal view {
+		super._preValidatePurchase(beneficiary, weiAmount);
+		require(weiAmount >= _minPurchase, "MarebitsPresale: sale amount must be greater than or equal to minimum purchase");
 	}
 }
---
+++ migrations/1631725013_marebits_presale.js
@@ -0,0 +1,41 @@
+const MarebitsPresale = artifacts.require("MarebitsPresale");
+const Migrations = artifacts.require("Migrations");
+
+const self = this;
+const URL = require("url").URL;
+const addresses = {
+	development: { ethWallet: "0x8145cb4a7d87020723b765b0985dae0569180700", tokenAddress: "0xae7F929A0aDaB593Ff7ffe7Db38Bf4ACf8FCeFcd" }, 
+	main: { ethWallet: "0x00Ad9AEb02CC7892c94DBd9E9BE93Ec5cf644632", tokenAddress: "0xc5a1973e1f736e2ad991573f3649f4f4a44c3028" }, 
+};
+const saleCap = "3"; // max amount of ETH to be raised
+const saleRate = "65426600000"; // tokens per ETH
+const minPurchase = "1".padEnd(20 - saleRate.length, "0"); // minimum purchase amount, in wei
+const startTimeOffsetMinutes = 5; // minutes from beginning of deployment
+const endTimeOffsetMinutes = 60; // minutes from beginning of deployment
+const etherScanGasOracleURL = new URL("https://api.etherscan.io/api?module=gastracker&action=gasoracle");
+const gasLimit = 100000;
+
+const { ETHERSCAN_API_KEY } = require("../secrets.json");
+// const MareBitsABI = require("../MareBits ABI.json");
+const fetch = require("node-fetch");
+const now = new self.Date();
+const web3 = Migrations.interfaceAdapter.web3;
+etherScanGasOracleURL.searchParams.append("apikey", ETHERSCAN_API_KEY);
+
+async function getGas() {
+	const responseObject = await fetch(etherScanGasOracleURL);
+	const response = await responseObject.json();
+	return { gasPrice: { value: response.result.SafeGasPrice, writable: true }, gas: { value: gasLimit * self.Number.parseInt(response.result.SafeGasPrice), writable: true } };
+}
+
+module.exports = async function(deployer, network) {
+	const { ethWallet, tokenAddress } = addresses[network];
+	const startTime = self.Math.round((now.getTime() + startTimeOffsetMinutes * 60 * 1000) / 1000);
+	const endTime = startTime + endTimeOffsetMinutes * 60;
+	const options = { from: ethWallet };
+	// const MareBits = new web3.eth.Contract(MareBitsABI, tokenAddress);
+
+	self.Object.defineProperties(options, await getGas());
+	await deployer.deploy(MarebitsPresale, saleRate, ethWallet, tokenAddress, web3.utils.toWei(saleCap), startTime, endTime, minPurchase, options);
+	// await MareBits.methods.transfer(tokenAddress, saleCap).call(options);
+};
